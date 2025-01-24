7fb622bc02fde12abefef4d81ba64de1b05c8347
SufiyanAbbasi <sufiyantahirabbasi1122@gmail.com>
2025-01-24 15:50:28 +0500

    practice

--- src/app/pipes/country-code.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CountryCodePipe } from './country-code.pipe';
-
-describe('CountryCodePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CountryCodePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
